dbbf6f0c4676bcadaad326510dec2c9f2d45c01c
liuben <ben.liu@cue.group>
2021-06-07 15:22:35 +0800

    Feat ：【数组-最大子序和】

---
+++ src/com/lb/leetcode/数组/solution/寻找两个正序数组的中位数.java
@@ -0,0 +1,8 @@
+package com.lb.leetcode.数组.solution;
+
+/**
+ * @author: liuben
+ * @date: 2021/6/7
+ */
+public class 寻找两个正序数组的中位数 {
+}
---
+++ src/com/lb/leetcode/数组/solution/最大子序和.java
@@ -0,0 +1,40 @@
+package com.lb.leetcode.数组.solution;
+
+/**
+ * @author: liuben
+ * @date: 2021/6/7
+ */
+public class 最大子序和 {
+
+    // [-2,1,-3,4,-1,2,1,-5,4];
+    // 贪心算法
+    //public int maxSubArray(int[] nums) {
+    //    if (nums.length == 0) {
+    //        return nums[0];
+    //    }
+    //    int maxSubArrSum = nums[0];
+    //    int sum = nums[0];
+    //    for (int i = 1; i < nums.length; i++) {
+    //        sum = Math.max(sum, 0);
+    //        sum += nums[i];
+    //        maxSubArrSum = Math.max(sum, maxSubArrSum);
+    //    }
+    //    return maxSubArrSum;
+    //}
+
+    // 动态规划
+    public static int maxSubArray(int[] nums) {
+        int pre = 0, maxAns = nums[0];
+        for (int num : nums) {
+            pre = Math.max(pre + num, num);
+            maxAns = Math.max(maxAns, pre);
+        }
+        return maxAns;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println(最大子序和.maxSubArray(nums));
+    }
+
+}
