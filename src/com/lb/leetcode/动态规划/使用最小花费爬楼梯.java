a07d646e9e8cdca139ba767c2d7708aa149287bf
liuben <ben.liu@cue.group>
2021-07-06 14:02:47 +0800

    Fix ：...

--- src/com/lb/leetcode/动态规划/使用最小花费爬楼梯.java
+++ src/com/lb/leetcode/动态规划/使用最小花费爬楼梯.java
@@ -41,7 +41,7 @@ public class 使用最小花费爬楼梯 {
     /**
      * dp[i] = Min(dp[i-2]+cost[i-2],dp[i-1]+cost[i])
      */
-    public int minCostClimbingStairs(int[] cost) {
+    public static int minCostClimbingStairs(int[] cost) {
         if(cost == null){
             return 0 ;
         }
@@ -50,11 +50,22 @@ public class 使用最小花费爬楼梯 {
         }
         int n = cost.length;
         int[] dp = new int[n + 1];
-        dp[0] = dp[1] = 0;
         for (int i = 2; i <= n; i++) {
             dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
         }
         return dp[n];
+    }
+    //public static int minCostClimbingStairs(int[] cost) {
+    //    int dp[] = new int[cost.length+1];
+    //    for (int i = 2; i <= dp.length; i++) {
+    //        dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+    //    }
+    //    return dp[cost.length ];
+    //}
+
+    public static void main(String[] args) {
+        int[] cost = {1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
+        System.out.println(使用最小花费爬楼梯.minCostClimbingStairs(cost));
     }
 
 }
