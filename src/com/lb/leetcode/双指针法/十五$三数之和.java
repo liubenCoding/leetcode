bcdd7073123d804a5a3dba94c3f9711a9f7914da
liuben <ben.liu@cue.group>
2021-08-13 18:52:13 +0800

    Fix ：三数之和

--- src/com/lb/leetcode/双指针法/十五$三数之和.java
+++ src/com/lb/leetcode/双指针法/十五$三数之和.java
@@ -11,40 +11,51 @@ import java.util.List;
 public class 十五$三数之和 {
 
     public static List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
+        // 找出a + b + c = 0
+        // a = nums[i], b = nums[left], c = nums[right]
         for (int i = 0; i < nums.length; i++) {
+            // 排序之后如果第一个元素已经大于零，那么无论如何组合都不可能凑成三元组，直接返回结果就可以了
             if (nums[i] > 0) {
-                return res;
+                return result;
             }
-            // 数字相同，没必要再算了，一样的结果
+            // 错误去重方法，将会漏掉-1,-1,2 这种情况
+            /*
+            if (nums[i] == nums[i + 1]) {
+                continue;
+            }
+            */
+            // 正确去重方法
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
             int left = i + 1;
             int right = nums.length - 1;
-            // 找出nums[i]开始所有的组合情况
             while (right > left) {
+                // 去重复逻辑如果放在这里，0，0，0 的情况，可能直接导致 right<=left 了，从而漏掉了 0,0,0 这种三元组
+                /*
+                while (right > left && nums[right] == nums[right - 1]) right--;
+                while (right > left && nums[left] == nums[left + 1]) left++;
+                */
                 if (nums[i] + nums[left] + nums[right] > 0) {
                     right--;
                 } else if (nums[i] + nums[left] + nums[right] < 0) {
                     left++;
                 } else {
-                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    //// 去重逻辑应该放在找到一个三元组之后
-                    while (right > left && nums[right] == nums[right - 1]) {
-                        right--;
-                    }
-                    while (right > left && nums[left] == nums[left + 1]) {
-                        left++;
-                    }
-                    //// 找到答案时，双指针同时收缩
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    // 去重逻辑应该放在找到一个三元组之后
+                    while (right > left && nums[right] == nums[right - 1]) right--;
+                    while (right > left && nums[left] == nums[left + 1]) left++;
+
+                    // 找到答案时，双指针同时收缩
                     right--;
                     left++;
                 }
             }
+
         }
-        return res;
+        return result;
     }
 
     public static void main(String[] args) {
